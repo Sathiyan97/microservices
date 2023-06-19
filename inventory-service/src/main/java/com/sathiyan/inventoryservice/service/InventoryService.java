8eeb35f87da4996edca7e824045f80c8bdc896d2
Sathiyan <sathiyan97@gmail.com>
2023-06-19 17:20:32 +0530

    microservices integration done

---
+++ inventory-service/src/main/java/com/sathiyan/inventoryservice/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.sathiyan.inventoryservice.service;
+
+import com.sathiyan.inventoryservice.repository.InventoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+
+    }
+
+}
