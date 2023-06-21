3e2f7e93b75ba391fb58d8cdaf21b8b377cf51a7
Sathiyan <sathiyan97@gmail.com>
2023-06-21 10:09:48 +0530

    Inventory respone class added

--- inventory-service/src/main/java/com/sathiyan/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/sathiyan/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.sathiyan.inventoryservice.service;
 
+import com.sathiyan.inventoryservice.dto.InventoryResponse;
 import com.sathiyan.inventoryservice.repository.InventoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -12,9 +15,15 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
-
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(
+                        inventory ->
+                                InventoryResponse.builder()
+                                        .skuCode(inventory.getSkuCode())
+                                        .isInStock(inventory.getQuantity() > 0)
+                                        .build()
+                ).toList();
     }
 
 }
