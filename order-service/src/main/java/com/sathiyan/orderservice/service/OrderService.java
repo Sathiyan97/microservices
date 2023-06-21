8ad656eb67137e87a7c539f388a6fd9e293721e7
Sathiyan <sathiyan97@gmail.com>
2023-06-21 16:15:13 +0530

    all the application properties are changed for the discovery client.

--- order-service/src/main/java/com/sathiyan/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/sathiyan/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,8 +34,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
         //call inventory service, and place the order if product  is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8083/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build() .get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
