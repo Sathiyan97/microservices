f3931f490e22de626ff6f3cd6017e9504c75b89b
Sathiyan <sathiyan97@gmail.com>
2023-06-25 02:02:43 +0530

    keyclock issuer path setted in the api-gateway property.

---
+++ api-gateway/src/main/java/org/sathiyan/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package org.sathiyan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
