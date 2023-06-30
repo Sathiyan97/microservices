066beaa1fe2a65353e54d2f5ed555ff911c1c764
Sathiyan <sathiyan97@gmail.com>
2023-06-30 16:29:42 +0530

    circuite breaker added.

--- order-service/src/main/java/com/sathiyan/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/sathiyan/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.sathiyan.orderservice.controller;
 
 import com.sathiyan.orderservice.dto.OrderRequest;
 import com.sathiyan.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Place Successfully";
     }
 
+    public String fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something went wrong, Please order after some time!";
+    }
 }
